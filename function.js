0659f89be849a7e692dfde63d15b2661428a150b
Mariusz Nowak <mariusz@medikoo.com>
2014-08-13 11:48:42 +0200

    Add `asProperty` and `asPlain` options

--- function.js
+++ function.js
@@ -7,8 +7,8 @@ var value  = require('es5-ext/object/valid-value')
   , resume = esniff.resume
   , collectNest = esniff.collectNest;
 
-module.exports = function (name) {
-	var l, names;
+module.exports = function (name/*, options*/) {
+	var l, names, options = Object(arguments[1]), asProperty = false, asPlain = true;
 	name = String(value(name));
 	names = name.split('.').map(function (prop) {
 		prop = prop.trim();
@@ -16,11 +16,17 @@ module.exports = function (name) {
 		return prop;
 	});
 	l = names.length;
+	if (options.asProperty != null) asProperty = options.asProperty;
+	if (options.asPlain != null) asPlain = options.asPlain;
 	return function (code) {
 		code = String(value(code));
 		return esniff(code, names[0][0], function (i, previous) {
 			var j = 0, prop;
-			if (previous === '.') return next();
+			if (previous === '.') {
+				if (!asProperty) return next();
+			} else if (!asPlain) {
+				return next();
+			}
 			while (j < l) {
 				prop = names[j];
 				if (code.indexOf(prop, i) !== i) return next();
