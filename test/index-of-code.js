f76e79667b31b37970c1880c66e05ad55a0cfd63
Mariusz Nowak <mariusz@medikoo.com>
2014-08-13 09:38:59 +0200

    Remove obsolete code

--- test/index-of-code.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (t, a) {
-	var code = ' /* sdfsdfs */  // fefefefe\n  /*sdfsd */foo() /* amr */ bar()';
-	a(code.slice(t(code)), 'foo() /* amr */ bar()');
-	a(code.slice(t(code, 14)), 'foo() /* amr */ bar()');
-	a(code.slice(t(code, 45)), 'bar()');
-};
