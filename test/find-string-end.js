df352e9de6e46b7bd937da7d0a3eaa366fe46c2e
Mariusz Nowak <mariusz@medikoo.com>
2014-08-13 09:18:20 +0200

    Remove obsolete modules

--- test/find-string-end.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function (t, a) {
-	var source = JSON.stringify('raz"dwa\'\'\\\\""\""elo\n\tsdfsdf\u3423sdfefe');
-	a(t(source), source.length - 1);
-	a(t(source + 'raz'), source.length - 1);
-};
