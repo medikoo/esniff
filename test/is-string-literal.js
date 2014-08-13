0b2a15a014d27fa55b66e85514f69b5381812f6b
Mariusz Nowak <mariusz@medikoo.com>
2014-08-13 17:10:13 +0200

    Improve test messages

--- test/is-string-literal.js
+++ test/is-string-literal.js
@@ -6,8 +6,8 @@ module.exports = function (t, a) {
 	a(t('\'\''), true, "Empty ' string");
 	a(t('"sdfsdf'), false, "Not finished \" string");
 	a(t('\'sdfsdf'), false, "Not finished ' string");
-	a(t('\'sdf\\\'fefeefe\\\\efefe\\n\\\\\\\'\\\'efef"" "sdfdfsdf\''), true, "Messy ' string");
-	a(t('"sdf\\"fefeefe\\\\efefe\\n\\\\\\"\\"efef\'\' \'sdfdfsdf"'), true, "Messy \" string");
+	a(t('\'sdf\\\'fefeefe\\\\efefe\\n\\\\\\\'\\\'efef"" "sdfdfsdf\''), true, "' string");
+	a(t('"sdf\\"fefeefe\\\\efefe\\n\\\\\\"\\"efef\'\' \'sdfdfsdf"'), true, "\" string");
 	a(t('  "sdf\\"fefeefe\\\\efefe\\n\\\\\\"\\"efef\'\' \'sdfdfsdf"'), false, "Starts with ws");
 	a(t('"sdf\\"fefeefe\\\\efefe\\n\\\\\\"\\"efef\'\' \'sdfdfsdf"  '), false, "Ends with ws");
 	a(t('34'), false, "Number");
