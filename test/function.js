5e6dfeda5bd4bc63dad37d5261c0bdab5bdc6494
Mariusz Nowak <mariusz@medikoo.com>
2014-08-21 15:28:49 +0200

    -- lint

--- test/function.js
+++ test/function.js
@@ -35,7 +35,7 @@ module.exports = function (t) {
 				d();
 			});
 		},
-		"Method": function (a, d) {
+		Method: function (a, d) {
 			readFile(pg + '/method.js', 'utf-8', function (err, str) {
 				var plainR, astR;
 				if (err) {
