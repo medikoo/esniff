3e8e802427be27bfef30db16cfbcf4fae661566f
Mariusz Nowak <mariusz@medikoo.com>
2014-08-22 16:53:01 +0200

    Seclude resolveSeparated logic

--- resolve-arguments.js
+++ resolve-arguments.js
@@ -1,21 +1,7 @@
 'use strict';
 
-var value  = require('es5-ext/object/valid-value')
-  , esniff = require('./')
+var resolveSeparated  = require('./resolve-separated');
 
-  , next = esniff.next;
-
-module.exports = function (argumentsString/*, limit*/) {
-	var args, fromIndex, limit = arguments[1] || Infinity;
-	argumentsString = String(value(argumentsString));
-	args = [];
-	fromIndex = 0;
-	esniff(argumentsString, ',', function (i, previous, nest) {
-		if (nest) return next();
-		if (args.push(argumentsString.slice(fromIndex, i)) === limit) return;
-		fromIndex = i + 1;
-		return next();
-	});
-	if (args.length < limit) args.push(argumentsString.slice(fromIndex));
-	return args;
+module.exports = function (code/*, limit*/) {
+	return resolveSeparated(code, ',', arguments[1]);
 };
--- resolve-concat.js
+++ resolve-concat.js
@@ -1,21 +1,7 @@
 'use strict';
 
-var value  = require('es5-ext/object/valid-value')
-  , esniff = require('./')
-
-  , next = esniff.next;
+var resolveSeparated  = require('./resolve-separated');
 
 module.exports = function (code/*, limit*/) {
-	var expressions, fromIndex, limit = arguments[1] || Infinity;
-	code = String(value(code));
-	expressions = [];
-	fromIndex = 0;
-	esniff(code, '+', function (i, previous, nest) {
-		if (nest) return next();
-		if (expressions.push(code.slice(fromIndex, i)) === limit) return;
-		fromIndex = i + 1;
-		return next();
-	});
-	if (expressions.length < limit) expressions.push(code.slice(fromIndex));
-	return expressions;
+	return resolveSeparated(code, '+', arguments[1]);
 };
