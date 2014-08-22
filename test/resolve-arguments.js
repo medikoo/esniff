a4192ac095fd9f6508ecb52733d3a1f8ba3d6883
Mariusz Nowak <mariusz@medikoo.com>
2014-08-22 16:21:23 +0200

    Improve tests

--- test/resolve-arguments.js
+++ test/resolve-arguments.js
@@ -4,5 +4,6 @@ module.exports = function (t, a) {
 	a.deep(t('"raz", "dwa", [\'raz\', \'dwa\'], "trzy"'),
 		['"raz"', ' "dwa"', ' [\'raz\', \'dwa\']', ' "trzy"']);
 	a.deep(t('"raz", "dwa", [\'raz\', \'dwa\'], "trzy"', 3),
-		['"raz"', ' "dwa"', ' [\'raz\', \'dwa\']']);
+		['"raz"', ' "dwa"', ' [\'raz\', \'dwa\']'], "Limit");
+	a.deep(t('"trzy"'), ['"trzy"'], "One argument");
 };
